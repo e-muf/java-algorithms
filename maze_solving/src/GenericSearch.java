1133472c1efa1d9ff041e9a73cee7ad7a4f95fc3
Emanuel Flores <emnl.fmtz@gmail.com>
2022-02-13 21:20:30 -0600

    DFS algorithm

--- maze_solving/src/GenericSearch.java
+++ maze_solving/src/GenericSearch.java
@@ -34,37 +34,6 @@ public class GenericSearch {
       return mine.compareTo(theirs);
     }
   }
-  
-  public static <T extends Comparable<T>> boolean linearContains(
-    List<T> list, T key
-  ) {
-    for (T item : list) {
-      if (item.compareTo(key) == 0) {
-        return true; // found a match
-      }
-    }
-    return false;
-  }
-
-  // assumes *list* is already sorted
-  public static <T extends Comparable<T>> boolean binaryContains(
-    List<T> list, T key
-  ) {
-    int low = 0;
-    int high = list.size() - 1;
-    while (low <= high) { // while there is still a search space
-      int middle = (low + high) / 2;
-      int comparison = list.get(middle).compareTo(key);
-      if (comparison < 0) {  // middle is < key
-        low = middle + 1;
-      } else if (comparison > 0) { // middle is > key 
-        high = middle - 1;
-      } else { // middle is equal to key
-        return true;
-      }
-    }
-    return false;
-  }
 
   public static <T> Node<T> dfs(T initial, Predicate<T> goalTest, Function<T, List<T>> successors) {
     // frontier is where we've yet to go
@@ -103,11 +72,5 @@ public class GenericSearch {
       path.add(0, node.state); // add to front
     }
     return path;
-  }
-
-  public static void main(String[] args) {
-    System.out.println(linearContains(List.of(1, 5, 15, 15, 15, 15, 20), 5));
-    System.out.println(binaryContains(List.of("a", "d", "e", "f", "z"), "f"));
-    System.out.println(binaryContains(List.of("john", "mark", "ronald"), "sheila"));
   }
 }
